a678f8d70595271c81fe9413b25b0e1b87b95495
shotaPro <9688wpva@gmail.com>
2022-04-19 12:25:02 +0900

    commit complete

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '4ff7a3d244ad064a2f78fbad16c6caf45b3b80d0',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '4ff7a3d244ad064a2f78fbad16c6caf45b3b80d0',
+            'dev_requirement' => false,
+        ),
+    ),
+);
