2f6b0fb9d0697f89684f67ccdf92a39d7e5fb8f9
shotaPro <9688wpva@gmail.com>
2022-04-19 13:06:06 +0900

    jqery fix

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ if ($getU->loggedIn() === true) {
 <head>
 	<meta charset="UTF-8" />
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.css" />
-	<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	<link rel="stylesheet" href="assets/css/style-complete.css" />
 </head>
 <!--Helvetica Neue-->
